778050893993f7e442c8727eadb89ef0fe17aa66
Erik Ekedahl <erik.ekedahl@avistainc.com>
2021-06-06 13:48:00 -0500

    added initial attemps at code.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 import pygame
 
+import player
 
-def main():
-    pass
+pygame.init()
 
 
 
@@ -12,4 +12,52 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+
+    pl = player.Player()
+
+    screen = pygame.display.set_mode((900, 500))
+
+    clock = pygame.time.Clock()
+
+    font = pygame.font.SysFont("arial", 16)
+    font_height = font.get_linesize()
+    event_text = []
+
+    if pygame.joystick.get_count() > 0:
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+        print("Joystick found and initialized!")
+        print("There are " + str(joystick.get_numaxes()) + " axes,")
+        print("There are " + str(joystick.get_numbuttons()) + " buttons,")
+        print("There are " + str(joystick.get_numhats()) + " hats on this joystick")
+    else:
+        print("No joystick.")
+
+    run = True
+    while run:
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+#        pl._curX = newX
+#        pl._curY = newY
+
+        pl.draw_player(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type in (pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP):
+                print(str(event))
+
+
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- player.py
+++ player.py
@@ -1,1 +1,29 @@
 import pygame
+SQUARE = 0
+CIRCLE = 1
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+PINK = (255, 125, 125)
+
+class Player:
+    _color = BLUE
+    _curShape = SQUARE
+
+
+    def draw_player(self, surface):
+        if self._curShape == SQUARE:
+            pygame.draw.rect(surface,
+                             self._color,
+                             pygame.Rect(self._curX, self._curY, 10, 10))
+
+    def __init__(self):
+        self.color = RED
+        self._curShape = SQUARE
+        self._curX = 100
+        self._curY = 100
+
