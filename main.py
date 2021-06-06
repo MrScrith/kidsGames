8b35a8e4f579c82482b939025cd9b3ac73e4741a
Erik <erikofekedahl@gmail.com>
2021-06-06 18:17:53 -0500

    Updated to change color and size

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ if __name__ == '__main__':
         print("There are " + str(joystick.get_numhats()) + " hats on this joystick")
     else:
         print("No joystick.")
-
+    newX = 0
+    newY = 0
+    
     run = True
     while run:
 
@@ -40,17 +42,33 @@ if __name__ == '__main__':
 
         clock.tick(60)
 
-#        pl._curX = newX
-#        pl._curY = newY
+        newX = (joystick.get_axis(0).real * 5) + newX
+        newY = (joystick.get_axis(1).real * 5) + newY
+
+        pl._curX = newX
+        pl._curY = newY
+
 
         pl.draw_player(screen)
+
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            if event.type in (pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP):
+            if event.type in (pygame.JOYBALLMOTION, pygame.JOYHATMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP):
                 print(str(event))
+            if event.type == pygame.JOYBUTTONDOWN:
+                print(str(joystick.get_button(0)))
+                if joystick.get_button(0) == 1:
+                    pl.next_color()
+                elif joystick.get_button(3) == 1:
+                    pl.prev_color()
+                elif joystick.get_button(5) == 1:
+                    pl.inc_size()
+                elif joystick.get_button(4) == 1:
+                    pl.dec_size()
 
 
 
--- player.py
+++ player.py
@@ -10,16 +10,43 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 PINK = (255, 125, 125)
 
+
 class Player:
     _color = BLUE
+    _color_index = 0
     _curShape = SQUARE
+    _size = 10
+
+    _color_list = [BLUE, WHITE, BLACK, RED, YELLOW, GREEN, PINK]
+
+    def inc_size(self):
+        self._size += 5
+        if self._size >= 100:
+            self._size = 100
+
+    def dec_size(self):
+        self._size -= 5
+        if self._size <= 10:
+            self._size = 10
+
+    def next_color(self):
+        self._color_index += 1
+        if self._color_index >= len(self._color_list):
+            self._color_index = 0
+        self._color = self._color_list[self._color_index]
+
+    def prev_color(self):
+        self._color_index -= 1
+        if self._color_index == 0:
+            self._color_index = len(self._color_list) - 1
+        self._color = self._color_list[self._color_index]
 
 
     def draw_player(self, surface):
         if self._curShape == SQUARE:
             pygame.draw.rect(surface,
                              self._color,
-                             pygame.Rect(self._curX, self._curY, 10, 10))
+                             pygame.Rect(self._curX, self._curY, self._size, self._size))
 
     def __init__(self):
         self.color = RED
